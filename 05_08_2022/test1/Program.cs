0d5fa2eb33b6a437e39f5bc159ace63c337d047b
Ulan <ulan.0110@gmail.com>
2022-08-06 01:25:14 +0600

    C# first impression

---
+++ 05_08_2022/test1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите квадрат числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1/num2 == num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
