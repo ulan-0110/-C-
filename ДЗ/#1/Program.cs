c510406709b9b122ec5eb1c55e234102bbf7adb6
Ulan <ulan.0110@gmail.com>
2022-08-09 00:39:38 +0600

    Homework 1

---
+++ ДЗ/#1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 > num1)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(num2);   
+}
+else
+{
+     Console.Write("Максимальное число: ");
+    Console.WriteLine(num1);
+}
